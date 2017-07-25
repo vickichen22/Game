2f6231f3a2e87348749a5f143620e261183a5c24
Bonnie <Bonniellhwhite@gmail.com>
2017-07-25 14:25:34 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var game = new Phaser.Game(800,600,Phaser.CANVAS,'game.Div')
+var mainState = {
+    preload: function() {
+
+    },
+
+    create: function() {
+
+    },
+
+    update: function() {
+
+    },
+
+    render: function() {
+
+    }
+}
+
+game.state.add("mainState",mainState);
+game.state.start("mainState");

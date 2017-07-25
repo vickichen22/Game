6f9b0d4e3e7dbce22df9687a6080118d59c6025c
vickichen22 <vickichen000222@gmail.com>
2017-07-25 14:54:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var game = new Phaser.Game(800,600,Phaser.CANVAS,'game.Div')
 var mainState = {
     preload: function() {
-        this.load.image('background', 'https://s3.amazonaws.com/spoonflower/public/design_thumbnails/0424/5908/1431605648965_drop_preview.png'
+        this.load.image('background', 'https://s3.amazonaws.com/spoonflower/public/design_thumbnails/0424/5908/1431605648965_drop_preview.png');
       
     },
 
     create: function() {
-    	this.background = this.game.add.sprite(0, 0, 'background')
+    	this.background = this.game.add.sprite(0, 0, 'background');
         
         
     },

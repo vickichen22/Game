6488a4c4bff0804cf6c920a5e370663eb1fbcba7
vickichen22 <vickichen000222@gmail.com>
2017-07-25 14:48:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var game = new Phaser.Game(800,600,Phaser.CANVAS,'game.Div')
 var mainState = {
     preload: function() {
-
+        this.load.image('background', 'Pictures/city.png'
+      
     },
 
     create: function() {
-
+    	this.background = this.game.add.sprite(0, 0, 'background')
+        
+        
     },
 
     update: function() {
